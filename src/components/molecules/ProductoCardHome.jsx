849aa44b912982baed3f6209bc549f95738c8b54
AllanxSF <al.salinast@duocuc.cl>
2025-11-23 21:34:05 -0300

    home

--- src/components/molecules/ProductoCardHome.jsx
+++ src/components/molecules/ProductoCardHome.jsx
@@ -1,28 +1,39 @@
 
 import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-import "../../styles/components/molecules/ProductoCard.css";
+import "../../styles/components/molecules/ProductoCardHome.css";
 
-const ProductoCard = ({ producto }) => {
+const ProductoCardHome = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
   const handleAdd = () => {
     addToCart({
-      productoId: producto.productoId,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      imagenUrl: producto.imagenUrl, // adapta al nombre real
+      productoId: product.producto_id,
+      nombre: product.nombre,
+      precio: product.precio,
+      imagenUrl: product.imagenUrl,
     });
   };
 
   return (
-    <div className="producto-card">
-      {/* resto de tu card... */}
-      <button className="btn-add-cart" onClick={handleAdd}>
-        Añadir al carrito
+    <div className="producto-card-home">
+
+      <div className="producto-img-box">
+        <img src={product.imagenUrl} alt={product.nombre} />
+      </div>
+
+      <div className="producto-info-home">
+        <h3 className="producto-nombre">{product.nombre}</h3>
+        <p className="producto-precio-home">${product.precio}</p>
+      </div>
+
+      <button className="btn-add-home" onClick={handleAdd}>
+        Añadir <br /> al carrito
       </button>
+
     </div>
   );
 };
 
-export default ProductoCard;
+export default ProductoCardHome;
+
